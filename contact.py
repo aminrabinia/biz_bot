7fd58e6147ccd825e66b22c3c7bdac3c7663a20e
amin rabinia <amin.rabinia@gmail.com>
2023-07-05 10:36:39 -0700

    "added google sheet"

---
+++ contact.py
@@ -0,0 +1,20 @@
+class UserData:
+    def __init__(self):
+        self.name = ""
+        self.email = ""
+        self.car = ""
+
+    def get_user_info(self, name="", email="", car=""):
+        if name:
+            self.name = name
+        if email:
+            self.email = email
+        if car:
+            self.car = car
+
+        self.print_user_info()
+
+    def print_user_info(self):
+        print("Name:", self.name)
+        print("Email:", self.email)
+        print("Car:", self.car)
--- main.py
+++ main.py
@@ -1,25 +1,87 @@
-# import functions_framework
+
+import requests
 from fastapi import FastAPI
 import gradio as gr
 import os
 import openai
 import uvicorn
+import json
+import gspread 
+from contact import UserData
+from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv, find_dotenv
+
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+ 
+credentials = ServiceAccountCredentials.from_json_keyfile_name('glissai-ml-apis-3c1339a5caa4.json', scope)
+client = gspread.authorize(credentials)
+worksheet = client.open("Lexus_dealership").sheet1
+print("\n\nWriting contacts to", worksheet.title)
+
+user = UserData()
+
+functions=[
+    {
+        "name": "get_user_info",
+        "description": "Get users contact information and selected car.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},        
+                "email": {"type": "string"},
+                "car": {"type": "string", 
+                "description": "the type of car that user is interested in",
+                "enum": ["Lexus RX", "Lexus NX", "Lexus IS", "Lexus GX"]}
+            },
+            "required": [],
+        },
+    }
+]
+
+
 def get_completion_from_messages(messages, 
-                                 model="gpt-3.5-turbo", 
+                                 model="gpt-3.5-turbo-16k", 
                                  temperature=0, 
-                                 max_tokens=500):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-        max_tokens=max_tokens, # the maximum number of tokens the model can ouptut 
-    )
+                                 max_tokens=100):
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature, # this is the degree of randomness of the model's output
+            max_tokens=max_tokens, # the maximum number of tokens the model can ouptut 
+            functions=functions,
+            function_call="auto",
+        )
+    except requests.exceptions.RequestException as e:
+        print("Network error:", e)
+        return "Sorry, there is a technical issue on my side... \
+        please wait a few seconds and try again."
+
+    gpt_response = response["choices"][0]["message"]
+    print(gpt_response)
+
+    if gpt_response.get("function_call"):
+        function_name = gpt_response["function_call"]["name"]
+        if function_name == "get_user_info":
+	        arguments = json.loads(gpt_response["function_call"]["arguments"])
+	        user.get_user_info(
+	            name=arguments.get("name"),
+	            email=arguments.get("email"),
+	            car=arguments.get("car"),
+	        )
+	        worksheet.insert_row([user.name, user.email, user.car])
+        else:
+            print("get_user_info not activated")
+    else: 
+        print("No function activated")
     return response.choices[0].message["content"]
+
 
 
 product_information="""
@@ -38,13 +100,13 @@ Car inspection and repair
 
 """
 
-def process_user_message(user_input, all_messages, debug=False):
+def process_user_message(user_input, all_messages):
     delimiter = "```"
     # Step 4: Answer the user question
     system_message = f"""
-    You are a customer service assistant for a large car dealership. \
+    You are a customer service assistant for a Lexus car dealership. \
     Respond in a friendly and helpful tone, with concise answers from the relevant information available. \
-    Make sure to ask the user relevant follow-up questions.
+    Ask for user's selected car, name and email, one by one, to contact them later.
     """
     messages = [
         {'role': 'system', 'content': system_message},
@@ -53,7 +115,6 @@ def process_user_message(user_input, all_messages, debug=False):
     ]
 
     final_response = get_completion_from_messages(all_messages + messages)
-    if debug:print("Step 4: Generated response to user question.")
     all_messages = all_messages + messages[1:]
     
     return final_response, all_messages
@@ -71,13 +132,13 @@ chat_history = []
 
 print("\n===chatbot started======\n")
 with gr.Blocks(css="footer {visibility: hidden}") as demo:
-    chatbot = gr.Chatbot()
+    chatbot = gr.Chatbot([["","Hello from LEXUS Dealership! How can I help?"]])
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
 
     def respond(message, chat_history):
         global context
-        response, context = process_user_message(message, context, debug=False)
+        response, context = process_user_message(message, context)
         context.append({'role':'assistant', 'content':f"{response}"})
         chat_history.append((message, response))
         return "", chat_history
